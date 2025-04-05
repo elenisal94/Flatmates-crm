666c1096670caa233a3358f5d481891968e9a7e9
Eleni <57360206+elenisal94@users.noreply.github.com>
2025-04-05 07:48:12 +0100

    Clean up
    Small clean up before final app launch.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 const { Tenant, Task, RentPayment, BillPayment } = require("./schema.js");
 const tenantRoutes = require("./routes/tenantRoutes");
 const taskRoutes = require("./routes/taskRoutes");
