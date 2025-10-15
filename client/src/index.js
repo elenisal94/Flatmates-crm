b1e73b73356f932abc964a5a268ec9e8ee5948ed
Eleni <57360206+elenisal94@users.noreply.github.com>
2025-10-15 17:05:13 +0200

    Optimising for Auth0 callback

--- client/src/index.js
+++ client/src/index.js
@@ -24,7 +24,7 @@ root.render(
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin,
+        redirect_uri: window.location.origin + "/callback",
         audience: process.env.REACT_APP_AUTH0_AUDIENCE,
         scope: "openid profile email offline_access",
       }}
